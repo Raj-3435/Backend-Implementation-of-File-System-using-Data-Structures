3f61f38f3a90dc7e64227533e6a85d066fb27087
Raj-3435 <adityarajic1101@gmail.com>
2026-03-31 13:48:16 +0530

    Initial clean setup with Eclipse project

---
+++ src/com/uqedd/filesystem/TreeNode.java
@@ -0,0 +1,10 @@
+class TreeNode {
+    String name;
+    boolean isFile;
+    Map<String, TreeNode> children;
+    TreeNode parent;
+
+    TreeNode(String name, boolean isFile, TreeNode parent) {
+        // write this
+    }
+}
